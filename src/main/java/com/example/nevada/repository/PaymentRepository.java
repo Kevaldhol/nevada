f960fbcfac3cbf0f669663aeda7fa511d11f87bd
kevdhol <sYp41_41MLAWz2Y_fTi8>
2024-01-01 14:45:36 +0530

    add jpa function in payment repo

--- src/main/java/com/example/nevada/repository/PaymentRepository.java
+++ src/main/java/com/example/nevada/repository/PaymentRepository.java
@@ -1,5 +1,6 @@
 package com.example.nevada.repository;
 
+import com.example.nevada.dto.Status;
 import com.example.nevada.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +17,6 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
     Optional<BigDecimal> sumTodaysPaymentsAmountByUserId(@Param("userId") Long userId);
 
     Optional<Payment> findByAuthorizationCode(String authorizationCode);
+
+    Optional<List<Payment>> findByStatus(Status status);
 }
